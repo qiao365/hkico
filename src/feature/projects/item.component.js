be5337b30775caba298e659aee61a20ab34bdd04
liyp <w45372683@gmail.com>
2018-03-27 23:28:35 +0800

    edit num

--- src/feature/projects/item.component.js
+++ src/feature/projects/item.component.js
@@ -9,7 +9,17 @@ class ProjectItemComponent extends Component {
   render() {
     const { item } = this.props;
     const funded = item.completed / item.total * 100;
+    funded = funded.toFixed(4);
+    if(funded > 10) {
+      funded = Math.round(funded);
+    }else if(funded > 1) {
+      funded = Math.toFixed(2);
+    }
     const progressStyle = {width: funded + '%'};
+    const funedStyle = {color: '#F05F40'};
+    if(funded > 50) {
+      funedStyle.color: '#FFF'
+    }
     return (
       <div className="col-sm-12 col-md-4">
         <div className="thumbnail" onClick={this.onDetailClickHandle}>
@@ -24,11 +34,11 @@ class ProjectItemComponent extends Component {
             </div>
             <div className="progress">
               <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow={funded} aria-valuemin="0" aria-valuemax="100" style={progressStyle}>
-                {funded}%
+                <span className="progress-funded" style={funedStyle}>{funded}%</span>
               </div>
             </div>
             <div className="price">
-              <span><i>{item.completed}</i>已完成</span>
+              <span><i>{item.completed * 1}</i>已完成</span>
               <span></span>
             </div>
           </div>
